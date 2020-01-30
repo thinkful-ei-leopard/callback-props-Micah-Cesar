d9fd38b72b44cb4cdc165d0deeb3cae2dcc8ef03
Sushka-bit <micah.pai@outlook.com>
2020-01-30 14:06:05 -0600

    updated code

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Card.css';
-
 export default function Card(props) {
   return (
     <div className='Card'>
-      <button type='button'>
+      <button
+        type='button'
+        onClick={() => props.onClickDelete(props.id)}
+      >
         delete
       </button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
-  </div>
-  );
+    </div>
+  )
 }
 
+Card.propTypes = {
+  onClickDelete: () => {}
+}
